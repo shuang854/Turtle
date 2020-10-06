4b830b9960baf2a4db48f35568bf385cfe32f910
Simon Huang <shuang85497@gmail.com>
2020-10-06 18:30:26 -0400

    changed to dark theme

--- src/components/Player/GetExtension.tsx
+++ src/components/Player/GetExtension.tsx
@@ -8,11 +8,11 @@ const GetExtension: React.FC = () => {
   return (
     <IonContent class="extension-content">
       <IonListHeader>Browser extension installation is required.</IonListHeader>
-      <IonRouterLink href="https://addons.mozilla.org/en-US/firefox/addon/turtletv/" target="_blank" class="ext-link">
-        <IonImg src={firefox} alt="Get the extension for Firefox" class="firefox-ext-img"></IonImg>
-      </IonRouterLink>
       <IonRouterLink href="https://chrome.google.com/webstore/search/TurtleTv" target="_blank" class="ext-link">
         <IonImg src={chrome} alt="Get the extension for Chrome" class="chrome-ext-img"></IonImg>
+      </IonRouterLink>
+      <IonRouterLink href="https://addons.mozilla.org/en-US/firefox/addon/turtletv/" target="_blank" class="ext-link">
+        <IonImg src={firefox} alt="Get the extension for Firefox" class="firefox-ext-img"></IonImg>
       </IonRouterLink>
     </IonContent>
   );
